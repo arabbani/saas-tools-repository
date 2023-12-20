65b660817c99413373ca446e2cf591d3044f9527
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-20 10:10:29 +0530

    vercel analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Saas Tools - Find The Exact Saas Tool For Your Needs',
-  description: 'Saas Tools Collects & Organizes All The Best Saas Tools So You Can Find Them Easily In One Place',
-}
+  title: "Saas Tools - Find The Exact Saas Tool For Your Needs",
+  description:
+    "Saas Tools Collects & Organizes All The Best Saas Tools So You Can Find Them Easily In One Place",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
-  )
+  );
 }
