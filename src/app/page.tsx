f528ba7cc4463513cd16d55e9eb3a42e44f577d8
Arif Rabbani <arif.93rabbani@gmail.com>
2023-12-25 15:20:38 +0530

    Shadcn integration (#2)
    * shadcn added
    
    * inter font customisation
    
    * button component added
    
    * cleanup
    
    ---------
    
    Co-authored-by: Arif Rabbani <arif.rabbani@rksv.in>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="min-h-screen flex flex-col items-center justify-center gap-5 p-24">
       Saas tools
+      <Button>Shadcn</Button>
     </main>
   )
 }
