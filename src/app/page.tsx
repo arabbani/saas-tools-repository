ebacf541b0c59ee484343abb3e7eff2dd631470d
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-26 11:31:18 +0530

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui";
-
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-5 p-24">
       Saas tools
-      <Button>Shadcn</Button>
     </main>
-  )
+  );
 }
