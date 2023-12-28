4adc36a29bea98e688cef530650f1c5ec382f667
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-28 22:11:24 +0530

    card css optimisation

--- src/modules/tool/SaasToolCard.tsx
+++ src/modules/tool/SaasToolCard.tsx
@@ -14,22 +14,20 @@ import { SaasToolTags } from "./SaasToolTags";
 export function SaasToolCard({ tool }: { tool: SaasTool }) {
   return (
     <Card className="overflow-clip">
-      <CardContent className="grid grid-cols-12 h-36 lg:h-48 p-0">
+      <CardContent className="grid grid-cols-12 h-36 lg:h-40 p-0">
         <div className="col-span-5 relative">
           <Image src={tool.imageUrl} alt={tool.name} fill />
         </div>
         <div className="col-span-7 p-3 lg:p-4 flex flex-col">
-          <CardTitle className="text-link text-xs md:text-sm lg:text-base hover:text-linkeffect mb-2 flex gap-3 justify-between items-center">
+          <CardTitle className="text-link text-sm lg:text-base hover:text-linkeffect mb-2 flex gap-3 justify-between items-center">
             <Link href="/" className="underline hover:no-underline hyphens-auto">
               {tool.name}
             </Link>
             <a href={tool.websiteUrl} target="_blank">
-              <ExternalLinkIcon className="size-4" />
+              <ExternalLinkIcon className="size-5" />
             </a>
           </CardTitle>
-          <CardDescription className="grow text-xs lg:text-sm text-pretty">{`${tool.description
-            .substring(0, 150)
-            .trimEnd()}...`}</CardDescription>
+          <CardDescription className="grow text-pretty">{tool.excerpt}</CardDescription>
           <CardFooter className="p-0 pt-2 hidden lg:block">
             <SaasToolTags tags={tool.tags} />
           </CardFooter>
