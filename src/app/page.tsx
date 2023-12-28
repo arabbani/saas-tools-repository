d16f2f5a5828d05145977c71c953a7b9114b3f38
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-28 15:19:08 +0530

    retrigger build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { SaasToolList } from "@/modules/tool";
 
 export default async function Home() {
   const saasTools = await getSaasTools();
+  
   return (
     <main className="py-12 max-w-7xl mx-auto">
       <SaasToolList tools={saasTools} />
