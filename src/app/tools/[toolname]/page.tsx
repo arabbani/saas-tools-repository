ab21020fbd34ace8945d2e438fb5deae784d367f
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-29 00:11:56 +0530

    details page more design added

--- src/app/tools/[toolname]/page.tsx
+++ src/app/tools/[toolname]/page.tsx
@@ -1,5 +1,6 @@
 import { TypographyH2 } from "@/components/custom";
 import { findSaasToolByName } from "@/database/data";
+import { SaasToolPricingTag, SaasToolTags } from "@/modules/tool";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 
@@ -19,13 +20,18 @@ export default async function ToolDetails({ params }: Props) {
   }
 
   return (
-    <div className="max-w-5xl mx-auto grid grid-cols-12 gap-6 h-48">
-      <div className="col-span-5 relative">
+    <div className="max-w-5xl mx-auto grid grid-cols-12 gap-6">
+      <div className="col-span-5 relative max-h-64">
         <Image src={saasTool.imageUrl} alt={saasTool.name} fill />
       </div>
       <div className="col-span-7 relative">
         <TypographyH2>{saasTool.name}</TypographyH2>
-        <p className="mt-4 text-pretty">{saasTool.description}</p>
+        <p className="my-4 text-pretty">{saasTool.description}</p>
+        <p className="mb-4">
+          <span className="font-medium">Pricing:</span>{" "}
+          <SaasToolPricingTag pricingModel={saasTool.pricingModel} />
+        </p>
+        <SaasToolTags tags={saasTool.tags} />
       </div>
     </div>
   );
