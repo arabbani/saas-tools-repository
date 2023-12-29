a19834541c6a4ff66b595226527eca1b1e3b57a7
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-29 23:28:05 +0530

    checkbox label component update

--- src/client-components/CheckboxWithLabel.tsx
+++ src/client-components/CheckboxWithLabel.tsx
@@ -1,24 +1,22 @@
 "use client";
 
-import { Checkbox } from "@/components/ui";
+import { Checkbox, Label } from "@/components/ui";
 import { useId } from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 
 interface CheckboxWithLabelProps
   extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {}
 
-export function CheckboxWithLabel({ children, ...props }: CheckboxWithLabelProps) {
+export function CheckboxWithLabel({
+  children,
+  ...props
+}: CheckboxWithLabelProps) {
   const id = useId();
 
   return (
     <div className="flex items-center space-x-2 cursor-pointer">
       <Checkbox id={id} {...props} />
-      <label
-        htmlFor={id}
-        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 cursor-pointer"
-      >
-        {children}
-      </label>
+      <Label htmlFor={id} className="cursor-pointer">{children}</Label>
     </div>
   );
 }
