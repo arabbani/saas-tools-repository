3d1762d4b05a404649366c2540bb3f5b77279c06
Arif Rabbani <arif.rabbani@rksv.in>
2023-12-31 13:42:53 +0530

    filter applies once user click apply

--- src/components/client/SaasToolCategoryFilter.tsx
+++ src/components/client/SaasToolCategoryFilter.tsx
@@ -1,54 +1,28 @@
 "use client";
 
 import { SaasCategory } from "@/database/schema";
-import { useRouter, useSearchParams } from "next/navigation";
 import { CheckboxWithLabel } from "./CheckboxWithLabel";
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
   existingCategoryFilter: string[];
   categories: SaasCategory[];
+  handleValueChange: (checked: boolean, category: string) => void;
 }
 
 export function SaasToolCategoryFilter({
   existingCategoryFilter,
   categories,
+  handleValueChange,
 }: Props) {
-  const params = useSearchParams();
-  const router = useRouter();
-
-  const handleCategoryChange = (checked: boolean, category: string) => {
-    const newParams = new URLSearchParams(params.toString());
-    let selectedPricingModelFilter: string[] = [];
-
-    if (newParams.get("category")) {
-      selectedPricingModelFilter = newParams.get("category")!.split(",");
-    }
-
-    if (checked && !selectedPricingModelFilter.includes(category)) {
-      selectedPricingModelFilter.push(category);
-    } else {
-      selectedPricingModelFilter = selectedPricingModelFilter.filter(
-        (item) => item !== category
-      );
-    }
-
-    newParams.delete("category");
-
-    if (selectedPricingModelFilter.length) {
-      newParams.set("category", selectedPricingModelFilter.toString());
-    }
-    router.push(`/?${newParams.toString()}`);
-  };
-
   return (
     <div className="grid md:grid-cols-4 lg:grid-cols-6 md:gap-x-3 md:gap-y-4">
       {categories?.map((category) => (
         <CheckboxWithLabel
           key={category.id}
           onCheckedChange={(checked: boolean) => {
-            handleCategoryChange(checked, category.name);
+            handleValueChange(checked, category.name);
           }}
-          checked={existingCategoryFilter.includes(category.name)}
+          defaultChecked={existingCategoryFilter.includes(category.name)}
         >
           {category.name}
         </CheckboxWithLabel>
---
+++ src/components/client/SaasToolsFilterClientComponentsAggregator.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import {
+  SaasToolCategoryFilter,
+  SaasToolPricingModelFilter,
+} from "@/components/client";
+import { Button, Separator } from "@/components/ui";
+import { SaasCategory } from "@/database/schema";
+import { PricingModel } from "@/util/types";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useRef } from "react";
+
+type Props = {
+  existingPricingModelFilter: PricingModel[];
+  existingCategoryFilter: string[];
+  categories: SaasCategory[];
+};
+
+export async function SaasToolsFilterClientComponentsAggregator({
+  existingPricingModelFilter,
+  existingCategoryFilter,
+  categories,
+}: Props) {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const selectedFilter = useRef({
+    category: existingCategoryFilter,
+    pricingModel: existingPricingModelFilter,
+  });
+
+  const handlePricingModelChange = (
+    checked: boolean,
+    pricingModel: PricingModel
+  ) => {
+    handleFilterChange("pricingModel", checked, pricingModel);
+  };
+
+  const handleCategoryChange = (checked: boolean, pricingModel: string) => {
+    handleFilterChange("category", checked, pricingModel);
+  };
+
+  const applyFilter = () => {
+    const newParams = new URLSearchParams(params.toString());
+    newParams.delete("pricingModel");
+    newParams.delete("category");
+
+    if (selectedFilter.current.category.length) {
+      newParams.set("category", selectedFilter.current.category.toString());
+    }
+
+    if (selectedFilter.current.pricingModel.length) {
+      newParams.set(
+        "pricingModel",
+        selectedFilter.current.pricingModel.toString()
+      );
+    }
+    router.push(`/?${newParams.toString()}`);
+  };
+
+  const handleFilterChange = (
+    kind: "category" | "pricingModel",
+    checked: boolean,
+    key: string
+  ) => {
+    let newFilter: string[] = selectedFilter.current[kind];
+
+    if (checked && !newFilter.includes(key)) {
+      newFilter.push(key);
+    } else {
+      newFilter = newFilter.filter((item) => item !== key);
+    }
+    
+    if (kind === "pricingModel") {
+      selectedFilter.current[kind] = newFilter as PricingModel[];
+    } else {
+      selectedFilter.current[kind] = newFilter;
+    }
+  };
+
+  return (
+    <div>
+      <SaasToolCategoryFilter
+        categories={categories}
+        existingCategoryFilter={existingCategoryFilter}
+        handleValueChange={handleCategoryChange}
+      />
+      <Separator decorative className="my-3" />
+        <SaasToolPricingModelFilter
+          existingPricingModelFilter={existingPricingModelFilter}
+          handleValueChange={handlePricingModelChange}
+        />
+      <div className="flex justify-between">
+        <Button onClick={applyFilter}>Apply</Button>
+      </div>
+    </div>
+  );
+}
