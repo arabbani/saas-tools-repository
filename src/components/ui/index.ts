7967ad6126902eff5056851cf230d2a200b37d09
Arif Rabbani <arif.rabbani@rksv.in>
2024-01-01 13:37:57 +0530

    shadcn naviagtion menu added

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,6 @@ export * from "./button";
 export * from "./card";
 export * from "./checkbox";
 export * from "./label";
+export * from "./navigation-menu";
 export * from "./select";
 export * from "./separator";
