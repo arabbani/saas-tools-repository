4fddb73d0f9a5270e1bbf82360b0aec221d4349a
Arif Rabbani <arif.rabbani@rksv.in>
2024-01-02 11:22:12 +0530

    revert shadcn naviagtion menu added
    This reverts commit 7967ad6126902eff5056851cf230d2a200b37d09.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,5 @@ export * from "./button";
 export * from "./card";
 export * from "./checkbox";
 export * from "./label";
-export * from "./navigation-menu";
 export * from "./select";
 export * from "./separator";
